554e8a255255bfa24e6aa9f38d6a210be70abc6e
Charles <rhcharles0@gmail.com>
2025-12-27 09:15:27 +0900

    fix: 배포환경에서 cors 설정

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,29 @@
 import type { NextConfig } from 'next';
 
-// const proxyConfig = import('@/src/lib/proxy'); // 또는 import
-
-const NEXT_PUBLIC_FRONTEND_URL = process.env.NEXT_PUBLIC_FRONTEND_URL;
 const nextConfig: NextConfig = {
     /* config options here */
 
     async headers() {
+        const allowedOrigins = [
+            'http://localhost:3007',
+            // Vercel 환경 변수에서 프론트엔드 URL을 가져옵니다.
+            // 값이 없거나 undefined일 경우를 대비해 필터링합니다.
+            process.env.NEXT_PUBLIC_FRONTEND_URL,
+        ].filter(Boolean); // null, undefined, 빈 문자열 제거
         return [
             {
                 source: '/api/:path*',
                 headers: [
                     {
                         key: 'Access-Control-Allow-Origin',
-                        value: 'http://localhost:3007',
+                        value:
+                            process.env.NEXT_PUBLIC_FRONTEND_URL ||
+                            'http://localhost:3007',
                     },
                     {
-                        key: 'Access-Control-Allow-Origin',
-                        value: `${NEXT_PUBLIC_FRONTEND_URL}`,
+                        // 💡 Credentials (쿠키, 인증)를 주고받을 때 필수
+                        key: 'Access-Control-Allow-Credentials',
+                        value: 'true',
                     },
                     {
                         key: 'Access-Control-Allow-Methods',
