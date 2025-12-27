afd1350ffe32eff2f9669822be8386130ebf9001
Charles <rhcharles0@gmail.com>
2025-12-27 09:20:55 +0900

    fix: 강제로 주소 주입

--- next.config.ts
+++ next.config.ts
@@ -4,12 +4,6 @@ const nextConfig: NextConfig = {
     /* config options here */
 
     async headers() {
-        const allowedOrigins = [
-            'http://localhost:3007',
-            // Vercel 환경 변수에서 프론트엔드 URL을 가져옵니다.
-            // 값이 없거나 undefined일 경우를 대비해 필터링합니다.
-            process.env.NEXT_PUBLIC_FRONTEND_URL,
-        ].filter(Boolean); // null, undefined, 빈 문자열 제거
         return [
             {
                 source: '/api/:path*',
@@ -18,7 +12,7 @@ const nextConfig: NextConfig = {
                         key: 'Access-Control-Allow-Origin',
                         value:
                             process.env.NEXT_PUBLIC_FRONTEND_URL ||
-                            'http://localhost:3007',
+                            'https://file-extension-fe-1xtr-h25p04ptr-rhkth96-1006s-projects.vercel.app/',
                     },
                     {
                         // 💡 Credentials (쿠키, 인증)를 주고받을 때 필수
